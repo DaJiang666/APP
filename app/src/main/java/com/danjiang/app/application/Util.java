13d4751eea5bccb9265d799b7523d34e6cc5d147
zhangdanjiang <zhangdanjiang@didichuxing.com>
2017-12-14 16:11:08 +0800

    你好 添加一个类

---
+++ app/src/main/java/com/danjiang/app/application/Util.java
@@ -0,0 +1,13 @@
+package com.danjiang.app.application;
+
+/**
+ * Created by didi on 2017/12/14.
+ */
+
+public class Util {
+
+    public static String getString(){
+        
+        return "你好";
+    }
+}
